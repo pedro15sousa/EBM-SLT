86c60f674896b19492cf27d3a070bf58ab94a2af
Pedro Sousa <pedro.marquessousa@student.manchester.ac.uk>
2024-02-19 15:01:53 +0000

    First commit

---
+++ energy_funcs/cnn.py
@@ -0,0 +1,160 @@
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Swish(nn.Module):
+
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+    
+
+######### ConvNet #########
+class ConvNet(nn.Module):
+    def __init__(self, in_chan=1, out_chan=64, nh=8):
+        """
+        ConvNet tailored for MNIST (28x28 images)
+        nh: multiplier for the number of filters
+        """
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(in_chan, nh * 4, kernel_size=3, bias=True)
+        self.conv2 = nn.Conv2d(nh * 4, nh * 8, kernel_size=3, bias=True)
+        self.max1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(nh * 8, nh * 8, kernel_size=3, bias=True)
+        self.conv4 = nn.Conv2d(nh * 8, nh * 16, kernel_size=3, bias=True)
+        self.max2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv5 = nn.Conv2d(nh * 16, out_chan, kernel_size=4, bias=True)
+        self.in_chan, self.out_chan = in_chan, out_chan
+        self.out_activation = Swish()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.max1(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.max2(x)
+        x = self.conv5(x)
+        return x
+    
+
+######### Original CNN from pre-trained model. #########
+class CNNModel(nn.Module):
+
+    def __init__(self, hidden_features=32, out_dim=1, **kwargs):
+        super().__init__()
+        # We increase the hidden dimension over layers. Here pre-calculated for simplicity.
+        c_hid1 = hidden_features//2
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+
+        # Series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4), # [18x18]
+                Swish(),
+                nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1), #  [9x9]
+                Swish(),
+                nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1), # [5x5]
+                Swish(),
+                nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1), # [3x3]
+                Swish(),
+                nn.Flatten(),
+                nn.Linear(c_hid3*4, c_hid3),
+                Swish(),
+                nn.Linear(c_hid3, out_dim)
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
+    
+
+
+######### First derivation of original CNN #########
+class SimpleCNNModel_1(nn.Module):
+
+    def __init__(self, hidden_features=16, out_dim=1, **kwargs):
+        super().__init__()
+        # Reduced hidden dimensions compared to the original model
+        c_hid1 = hidden_features // 2
+        c_hid2 = hidden_features
+
+        # Simplified series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4),  # [18x18]
+            Swish(),
+            nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1),  # [9x9]
+            Swish(),
+            nn.Flatten(),
+            nn.Linear(c_hid2*8*8, out_dim)  # Adjusted to match output dimension
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
+    
+
+######### Second derivation of original CNN #########
+class SimpleCNNModel_2(nn.Module):
+
+    def __init__(self, hidden_features=16, out_dim=1, **kwargs):
+        super().__init__()
+        # We increase the hidden dimension over layers. Here pre-calculated for simplicity.
+        c_hid1 = hidden_features//2
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+
+        # Series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4), # [18x18]
+                Swish(),
+                nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1), #  [9x9]
+                Swish(),
+                nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1), # [5x5]
+                Swish(),
+                nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1), # [3x3]
+                Swish(),
+                nn.Flatten(),
+                nn.Linear(c_hid3*4, c_hid3),
+                Swish(),
+                nn.Linear(c_hid3, out_dim)
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
+    
+
+######### Third derivation of original CNN #########
+class SimpleCNNModel_3(nn.Module):
+
+    def __init__(self, hidden_features=24, out_dim=1, **kwargs):
+        super().__init__()
+        # We increase the hidden dimension over layers. Here pre-calculated for simplicity.
+        c_hid1 = hidden_features//2
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+
+        # Series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4), # [18x18]
+                Swish(),
+                nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1), #  [9x9]
+                Swish(),
+                nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1), # [5x5]
+                Swish(),
+                nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1), # [3x3]
+                Swish(),
+                nn.Flatten(),
+                nn.Linear(c_hid3*4, c_hid3),
+                Swish(),
+                nn.Linear(c_hid3, out_dim)
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
